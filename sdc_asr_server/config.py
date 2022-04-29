f9c32ab7e6464e300ba8f24bd55d537d295ccef4
weiliang <weiliang.chong@day-care.cn>
2022-04-29 15:47:22 +0800

    replace argument with env

--- sdc_asr_server/config.py
+++ sdc_asr_server/config.py
@@ -1,42 +1,41 @@
 # -*- coding: utf-8 -*-
 '''
-server的相关配置均由运行参数赋值，省去配置文件，方便部署
+server的相关配置均由环境变量赋值，省去配置文件，方便部署
 '''
 
+import os
 import re
-import argparse
 
 
 
-ARGS = argparse.Namespace(
+CONF = dict(
     host='0.0.0.0',
     port=8300,
     ws=['ws://127.0.0.1:8301'],
 )
 
+
+def parse_env():
+    global CONF
+    CONF['host'] = os.getenv('SDC_HOST', CONF['host'])
+    CONF['port'] = int(os.getenv('SDC_PORT', CONF['port']))
+    ws = os.getenv('SDC_WS', '')
+    if ws:
+        ws = re.split(r'[,\s]+', ws)
+        for w in ws:
+            if not re.search(r':\d+'):
+                raise ValueError(f'invalid WS address {w}')
+        CONF['ws'] = ws
+
+
+parse_env()
 WS_INDEX = 0
 
 
 def get_ws():
     global WS_INDEX
-    if len(ARGS.ws) == 1:
-        return ARGS.ws[0]
-    idx = WS_INDEX / len(ARGS.ws)
+    if len(CONF['ws']) == 1:
+        return CONF['ws'][0]
+    idx = WS_INDEX / len(CONF['ws'])
     WS_INDEX = idx + 1
-    return ARGS.ws[idx]
-
-
-def parse_args():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--host', type=str, default='127.0.0.1', help="监听IP地址")
-    parser.add_argument('--port', type=int, default=8300)
-    parser.add_argument('--ws', type=str, default='127.0.0.1:8301', help="ASR websocket 地址, 多个以英文逗号分隔: ip1:port1,ip2:port2, ...")
-    args = parser.parse_args()
-    ws = re.split(r'[,\s]+', args.ws)
-    good = []
-    for w in ws:
-        if not w.startswith('ws://'):
-            w = 'ws://' + w
-        good.append(w)
-    args.ws = good
-    return args
+    return CONF['ws'][idx]
