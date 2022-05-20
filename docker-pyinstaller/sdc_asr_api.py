6ec02c68395b2721ee64798333dea5ed7b8fe842
weiliang <weiliang.chong@day-care.cn>
2022-05-20 16:49:42 +0800

    freeze_support() make sure exe not create so many processes

--- docker-pyinstaller/sdc_asr_api.py
+++ docker-pyinstaller/sdc_asr_api.py
@@ -1,8 +1,11 @@
-#!/home/weiliang/miniconda3/bin/python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 import re
 import sys
+import multiprocessing
 from asr_api_server.main import run
+
 if __name__ == '__main__':
+    multiprocessing.freeze_support()
     sys.argv[0] = re.sub(r'(-script\.pyw|\.exe)?$', '', sys.argv[0])
     sys.exit(run())
