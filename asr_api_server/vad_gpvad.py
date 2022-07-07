8c3606e62902049d5a3b9b4431ba9e54c8b0781b
weiliang <weiliang.chong@day-care.cn>
2022-07-07 13:44:45 +0800

    replace torch with onnxruntime

--- asr_api_server/vad_gpvad.py
+++ asr_api_server/vad_gpvad.py
@@ -1,6 +1,6 @@
 from io import BytesIO
 import soundfile as sf
-from asr_api_server.gpvad.forward import GPVAD
+from asr_api_server.gpvad_onnx.infer_onnxruntime import GPVAD
 from asr_api_server.logger import logger
 
 
@@ -22,7 +22,16 @@ def cut(timeline, data, samplerate):
 def vad(audio):
     bio = BytesIO(audio)
     timeline = VAD.vad(bio)
+    bio.seek(0)
     data, samplerate = sf.read(bio, dtype='int16')
     duration = len(data) / samplerate
     segments = cut(timeline, data, samplerate)
     return segments, duration, samplerate
+
+
+if __name__ == '__main__':
+    import sys
+    fp = sys.argv[1]
+    data = open(fp, 'rb').read()
+    s, d, sr = vad(data)
+    print(s, d, sr)
