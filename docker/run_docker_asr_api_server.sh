85036af21c7d1b5369d13adbbe48c9a86775bfbf
weiliang <weiliang.chong@day-care.cn>
2022-07-07 14:10:41 +0800

    upgrade version to 0.6.0

--- docker/run_docker_asr_api_server.sh
+++ docker/run_docker_asr_api_server.sh
@@ -1,1 +1,1 @@
-docker run --net=host -e WEB_CONCURRENCY=2 -d --name asr_api_server --restart=always asr_api_server:0.1.0
+docker run --net=host -e WEB_CONCURRENCY=2 -d --name asr_api_server --restart=always asr_api_server:0.6.0
