e154df3251202f6461bd9dc5d9c7cfd74c411625
weiliang <weiliang.chong@day-care.cn>
2022-07-12 09:13:21 +0800

    add rec_no_vad()

--- asr_api_server/asr_process.py
+++ asr_api_server/asr_process.py
@@ -4,6 +4,8 @@ import asyncio
 import time
 import aiohttp
 import traceback
+from io import BytesIO
+import soundfile as sf
 from asr_api_server import vad_gpvad as vad
 from asr_api_server.logger import logger
 from asr_api_server.ws_query import ws_rec
@@ -27,7 +29,27 @@ async def download(url):
     return data, msg
 
 
-async def rec(audio_origin):
+async def rec_no_vad(audio_origin):
+    b = time.time()
+    # with open('z-origin.wav', 'wb') as f:
+    #     f.write(audio_origin)
+    bio = BytesIO(audio_origin)
+    data, samplerate = sf.read(bio, dtype='int16')
+    try:
+        text = await ws_rec(data.astype('int16').tobytes())
+        exception = 0
+    except Exception as e:
+        logger.debug(e)
+        traceback.print_exc()
+        text = ''
+        exception = 1
+    timing = time.time() - b
+    logger.info(f'REC without VAD: time use:{timing}, {exception=}')
+    text = re.sub(r'<.*?>', '', text)
+    return text, exception
+
+
+async def rec_vad(audio_origin):
     b = time.time()
     # with open('z-origin.wav', 'wb') as f:
     #     f.write(audio_origin)
@@ -56,7 +78,14 @@ async def rec(audio_origin):
             exception += 1
         results.append(result)
     timing = time.time() - b
-    logger.info(f'REC: duration: [{duration}] seconds, time use:{timing}, max len of segment: {max_len}, segments_count: {segments_count}, {exception=}')
+    msg = (f'REC: duration: [{duration}] seconds, '
+           f'time use:{timing}, max len of segment: {max_len}, '
+           f'segments_count: {segments_count}, {exception=}')
+    logger.info(msg)
     text = ','.join([r['text'] for r in results])
     text = re.sub(r'<.*?>', '', text)
     return text, exception
+
+
+async def rec(audio_origin):
+    return rec_no_vad(audio_origin)
