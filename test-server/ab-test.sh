db6bcdc5218a08693a066d9424216cee5cc0a050
weiliang <weiliang.chong@day-care.cn>
2022-07-28 08:51:26 +0800

    change audio-url

--- test-server/ab-test.sh
+++ test-server/ab-test.sh
@@ -1,6 +1,6 @@
-ab -n 150 -c 50 -p ab_post.txt \
+ab -n 20 -c 10 -p ab_post.txt \
 	-H "accept: application/json" \
 	-H "appkey: 123" \
 	-H "format: pcm" \
-	-H "audio-url: https://ahc-audio.oss-cn-shanghai-internal.aliyuncs.com/video/SHC0011911260005/1759040041446465536/1629860409787.wav" \
+	-H "audio-url: http://127.0.0.1:8010/z-60s.wav" \
 	http://127.0.0.1:8300/asr/v1/rec
