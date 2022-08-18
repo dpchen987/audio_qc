facfd167246e370394edf0fcccffe271604c828e
weiliang <weiliang.chong@day-care.cn>
2022-08-18 17:39:50 +0800

    merge too short segment

--- asr_api_server/vad_gpvad.py
+++ asr_api_server/vad_gpvad.py
@@ -1,5 +1,6 @@
 from io import BytesIO
 import soundfile as sf
+import numpy as np
 from asr_api_server.gpvad_onnx.infer_onnxruntime import GPVAD
 from asr_api_server.logger import logger
 
@@ -9,15 +10,22 @@ VAD = GPVAD('sre')
 
 def cut(timeline, data, samplerate):
     segments = []
-    for tl in timeline:
-        if tl[1] - tl[0] < 500:
-            logger.info(f'too short audio segment {tl[1]-tl[0]}')
-            continue
+    last_duration = 0
+    min_duration = 1000  # 1s
+    max_duration = 10000  # 10s
+    for i, tl in enumerate(timeline):
+        duration = tl[1] - tl[0]
         start = int(tl[0] / 1000 * samplerate)
         end = int(tl[1] / 1000 * samplerate)
         segment = data[start: end]
-        # logger.info(f'segment: {start, end}')
-        segments.append(segment)
+        if segments and duration < min_duration and last_duration < max_duration:
+            # this seg append to last seg
+            segments[-1] = np.append(segments[-1], segment)
+            last_duration += duration
+        else:
+            segments.append(segment)
+            last_duration = duration
+    print(f'===== vad: {len(timeline) = }, {len(segments) = }')
     return segments
 
 
