df62f70f8bf2089e6d562972ac3400b052d3e0c4
weiliang <weiliang.chong@day-care.cn>
2022-09-05 13:29:21 +0800

    add for leveldb

--- docker/run_docker_asr_api_server.sh
+++ docker/run_docker_asr_api_server.sh
@@ -1,3 +1,3 @@
 version=$(cat ../asr_api_server/__init__.py | awk '{print $3}' | tr -d "'")
 echo "version: $version"
-docker run --net=host -e WEB_CONCURRENCY=2 -d --name asr_api_server --restart=always asr_api_server:$version
+docker run --net=host -e ASR_API_URL_DB=/app/url.db -v /path-in-host/:/app -d --name asr_api_server --restart=always asr_api_server:$version
