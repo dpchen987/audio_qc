36488beeb26f08c54cb31cd930e808b3fe103f96
weiliang <weiliang.chong@day-care.cn>
2022-09-07 11:32:42 +0800

    add ignore chars for CER computing

--- test-server/performance-api.py
+++ test-server/performance-api.py
@@ -20,6 +20,7 @@ async def test_coro(audio_data, api):
         async with session.post(api, data=audio_data, headers=headers) as resp:
             text = await resp.text()
     time_cost = time.time() - begin
+    text = json.loads(text)['text']
     return {
         'text': text,
         'time': time_cost,
@@ -38,6 +39,9 @@ def get_args():
     parser.add_argument(
         '-t', '--trans', required=True,
         help='path to trans_text_file of wavs')
+    parser.add_argument(
+        '-i', '--ignore', required=True,
+        help='path to ignore words file')
     parser.add_argument(
         '-s', '--save_to', required=True,
         help='path to save transcription')
@@ -93,24 +97,31 @@ async def main(args):
             request_times.append(result['time'])
     request_time = time.time() - begin
     rtf = request_time / total_duration
-    print('For all concurrence:')
-    print_result({
+    concur_info = {
         'failed': failed,
         'total_duration': total_duration,
         'request_time': request_time,
         'RTF': rtf,
-    })
-    print('For one request:')
-    print_result({
+    }
+    request_info = {
         'mean': statistics.mean(request_times),
         'median': statistics.median(request_times),
         'max_time': max(request_times),
         'min_time': min(request_times),
-    })
+    }
+    print('For all concurrence:')
+    print_result(concur_info)
+    print('For one request:')
+    print_result(request_info)
+    with open(args.save_to + '.log', 'w', encoding='utf8') as flog:
+        flog.write(json.dumps(concur_info, indent=4))
+        flog.write('\n')
+        flog.write(json.dumps(request_info, indent=4))
     with open(args.save_to, 'w', encoding='utf8') as fsave:
         fsave.write(''.join(texts))
     # caculate CER
-    cmd = (f'python ../compute-wer.py --char=1 --v=1 '
+    cmd = (f'python ../test-model/compute-wer.py --char=1 --v=1 '
+           f'--ig={args.ignore} '
            f'{args.trans} {args.save_to} > '
            f'{args.save_to}-test-{args.num_concurrence}.cer.txt')
     print(cmd)
