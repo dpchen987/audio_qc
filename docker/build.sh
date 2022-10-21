52c618be69f49c6742a3ee82c5c38b92b8d74dba
weiliang <weiliang.chong@day-care.cn>
2022-10-21 14:36:58 +0800

    optimize docker building

--- docker/build.sh
+++ docker/build.sh
@@ -1,31 +1,38 @@
+#!/bin/bash
 # get version
-version=`python get_version.py`
+
+version=$(python get_version.py)
 echo "version: $version"
 
 # build asr_api_server.*.whl if not exists here
-if [ -e asr_api_server-$version-cp38-cp38-linux_x86_64.whl ]; then
-    echo '---------------------------------------'
-    echo 'use old asr_api_server-$version-cp38-cp38-linux_x86_64.whl'
+if [ -e asr_api_server-"$version"-cp38-cp38-linux_x86_64.whl ]; then
+    echo '!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!'
+    echo "use old asr_api_server-$version-cp38-cp38-linux_x86_64.whl"
     echo 'if want to build new one, please delete the old one'
-    echo '---------------------------------------'
+    echo '!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!'
 else
     cd ..
     python setup.py bdist_wheel
-    find . -name *.c | xargs rm
-    cp dist/asr_api_server-$version-cp38-cp38-linux_x86_64.whl docker/
+    find . -print0 -name "*.c" | xargs rm
+    cp dist/asr_api_server-"$version"-cp38-cp38-linux_x86_64.whl docker/
     cd docker
 fi
 
-# download pip package to 'pip-pkg' if they are not there
-if [ -e pip-pkg ]; then
-    echo 'has pip-pkg'
+# check docker experimental and build
+is_experimental=$(docker system info | grep Experimental | grep true)
+echo "is_experimental: ($is_experimental)"
+if [ -n "$is_experimental" ]; then
+    printf "\n============================================================\n"
+    echo "Docker run with experimental will get smaller size of image"
+    echo "============================================================"
+    docker build --build-arg version="$version" --squash -t asr_api_server:"$version" .
 else
-    mkdir pip-pkg
+    printf "\n============================================================\n"
+    echo "Docker run without experimental will get bigger size of image"
+    echo "To get smaller image, please configure 'experimental' as true"
+    echo "============================================================"
+    docker build --build-arg version="$version" -t asr_api_server:"$version" .
 fi
-pip download -r requirements.txt -d pip-pkg
-
-# build docker
-docker build --build-arg version=$version -t asr_api_server:$version .
 
 
 # save docker image, give it to IT guys for deploying
