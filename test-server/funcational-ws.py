29f271d0c6f24d918a4db6be7b2edc57697a3452
weiliang <weiliang.chong@day-care.cn>
2022-12-01 10:17:46 +0800

    fix

--- test-server/funcational-ws.py
+++ test-server/funcational-ws.py
@@ -6,16 +6,20 @@ import time
 import asyncio
 import sys
 sys.path.append('../')
+import soundfile as sf
 
-from asr_api_server.ws_query import ws_rec
+from performance_ws import ws_rec
 
 
 async def main(fn):
+    ws_uri = 'ws://127.0.0.1:8303'
     print('start...')
-    with open(fn, 'rb') as f:
-        data = f.read()
+    # with open(fn, 'rb') as f:
+    #     data = f.read()
+    data, sr = sf.read(fn, dtype='int16')
+    data = data.tobytes()
     b = time.time()
-    text = await ws_rec(data)
+    text = await ws_rec(data, ws_uri)
     print(text)
     print(f'{len(text) = }', time.time() - b)
 
