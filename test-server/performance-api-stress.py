8f5db3ae81aa6308417cdd594b94bc50d7a5aa41
weiliang <weiliang.chong@day-care.cn>
2023-04-25 15:20:47 +0800

    fix

--- test-server/performance-api-stress.py
+++ test-server/performance-api-stress.py
@@ -15,22 +15,26 @@ def get_args():
     parser = argparse.ArgumentParser(description='')
     parser.add_argument(
         '-u', '--api_uri',
-        default='http://127.0.0.1:8300/asr/v1/rec',
+        default='http://127.0.0.1:8300/asr/v1/speech_rec',
         help="asr_api_server's uri, default: http://127.0.0.1:8300/asr/v1/speech_rec")
     parser.add_argument(
         '-f', '--audio_list', required=True,
         help='path to file of audio url list')
     parser.add_argument(
-        '-n', '--num_concurrence', type=int, required=True,
+        '-c', '--concurrence', type=int, required=True,
         help='num of concurrence for query')
+    parser.add_argument(
+        '-n', '--num_query', type=int, default=0,
+        help='num of total query')
     args = parser.parse_args()
     return args
 
 
-async def test_coro(api, audio_url, taskid, result):
+async def test_coro(api, taskid, audio_url, result):
     begin = time.time()
     query = {
             'task_id': taskid,
+            'callback_url': 'http://127.0.0.1:8304/asr/v1/callBack_test',
             'enable_punctution_prediction': True,
             'file_path': audio_url,
             'file_content': 'abc',
@@ -42,6 +46,7 @@ async def test_coro(api, audio_url, taskid, result):
             text = await resp.text()
     end = time.time()
     result.append({
+        'taskid': taskid,
         'begin': begin,
         'end': end,
         'text': text
@@ -63,24 +68,26 @@ async def main(args):
     begin = time.time()
     now = time.strftime('%Y-%m-%d_%H:%M:%S')
     for i, audio_url in enumerate(audio_urls):
+        if args.num_query and args.num_query < i:
+            break
         task_id = now + f'{i:012}'
         task = asyncio.create_task(
-                test_coro(args.api_uri, task_id, result))
+                test_coro(args.api_uri, task_id, audio_url, result))
         tasks.add(task)
         task.add_done_callback(tasks.discard)
-        if len(tasks) < args.num_concurrence:
+        if len(tasks) < args.concurrence:
             continue
-        if i % args.num_concurrence == 0:
-            print((f'{i=}, start {args.num_concurrence} '
+        if i % args.concurrence == 0:
+            print((f'{i=}, start {args.concurrence} '
                    f'queries @ {time.strftime("%m-%d %H:%M:%S")}'))
         await asyncio.sleep(0.05)
-        while len(tasks) >= args.num_concurrence:
+        while len(tasks) >= args.concurrence:
             await asyncio.sleep(0.05)
     while tasks:
         await asyncio.sleep(0.1)
     with open(f'{now}.log', 'w') as f:
-        json.dump(result, f)
-    print('done')
+        json.dump(result, f, indent=2)
+    print('done', time.time() - begin)
 
 
 if __name__ == '__main__':
