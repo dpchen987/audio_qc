c16ccc3df14a61c8487e3701af3d72e4095bce52
weiliang <weiliang.chong@day-care.cn>
2023-12-12 17:11:21 +0800

    isolated speech_vad from asr_consumer

--- asr_api_server/asr_consumer.py
+++ asr_api_server/asr_consumer.py
@@ -11,8 +11,8 @@ import base64
 from asr_api_server import asr_process
 from asr_api_server.logger import logger
 from asr_api_server import config
+from asr_api_server import urldb
 from asr_api_server.data_model.api_model import AudioInfo
-from asr_api_server.vad_gpvad import vad_duration
 
 ASR_NUM = None
 CALLBACK_URL = ""
@@ -28,7 +28,7 @@ async def asy_timer():
     logger.info("-----Regular inspection tasks execution ...-----")
     logger.info(f"CallBack url:{CALLBACK_URL}")
     # logger.info(f"tasks under processing: {config.processing_set}")
-    for task_id, inf_dict in config.url_db.RangeIter():
+    for task_id, inf_dict in urldb.url_db.RangeIter():
         if task_id.decode() not in config.processing_set and CALLBACK_URL:
             try:
                 audio_info = AudioInfo(**json.loads(inf_dict.decode()))
@@ -36,7 +36,7 @@ async def asy_timer():
                 asyncio.create_task(speech_recognize(audio_info))
                 logger.info(f"Recreate task：-----{task_id.decode()} ！！！----")
                 config.processing_set.add(task_id.decode())
-                config.url_db.Delete(task_id)
+                urldb.url_db.Delete(task_id)
             except Exception as e:
                 logger.exception(f"asy_timer: {e}")
     asyncio.create_task(asy_timer())
@@ -96,7 +96,7 @@ async def speech_recognize(audio_info):
             if resp_dt["code"] == 0:
                 # 识别完成，清理数据库
                 # if os.path.exists(audio_info.file_path): os.remove(audio_info.file_path)
-                config.url_db.Delete(audio_info.task_id.encode())
+                urldb.url_db.Delete(audio_info.task_id.encode())
             else:
                 logger.info(f"回调失败！！{resp_dt}")
                 logger.info(f"{html}")
@@ -107,37 +107,3 @@ async def speech_recognize(audio_info):
         finally:
             # 不管回调是否成功，都删除processing_set中的task
             config.processing_set.discard(audio_info.task_id)
-
-VAD_NUM = asyncio.Semaphore(10)
-async def speech_vad(audio_info):
-    '''识别语音为文本，接收语音数据audio-url参数，返回转译文本
-    '''
-    vad_result = {"code":0, "data": "0", "msg":"success"}
-    try:
-        async with VAD_NUM:
-            # 音频下载
-            if audio_info.trans_type == 1:
-                audio, msg = await asr_process.download(audio_info.file_path)
-            else:
-                # with open(audio_info.file_path, 'rb') as f:
-                #     audio = f.read()
-                audio = base64.b64decode(audio_info.file_content)
-                msg = 'ok' if audio else 'audio file is empty !!!'
-            if msg != 'ok':
-                vad_result['code'] = 4003
-                vad_result['msg'] = msg
-                return
-            if not audio:
-                vad_result['code'] = 4002
-                vad_result['msg'] = 'no audio data'
-                return
-            # 音频vad
-            b = time.time()
-            total = vad_duration(audio)
-            logger.info(f'vad time: {time.time() - b}, {total = }')
-            vad_result['data'] = json.dumps(total)
-    except Exception as e:
-        logger.exception(e)
-        vad_result['code'] = 4000
-        vad_result['msg'] = str(e)
-    return vad_result
---
+++ asr_api_server/vad_processor.py
@@ -0,0 +1,43 @@
+import time
+import asyncio
+import base64
+import json
+from .vad_gpvad import vad_duration
+from .logger import logger
+
+VAD_NUM = asyncio.Semaphore(100)
+
+
+async def speech_vad(audio_info):
+    '''识别语音为文本，接收语音数据audio-url参数，返回转译文本
+    '''
+    vad_result = {"code":0, "data": "0", "msg":"success"}
+    try:
+        async with VAD_NUM:
+            # 音频下载
+            if audio_info.trans_type == 1:
+                audio, msg = await asr_process.download(audio_info.file_path)
+            else:
+                # with open(audio_info.file_path, 'rb') as f:
+                #     audio = f.read()
+                b = time.time()
+                audio = base64.b64decode(audio_info.file_content)
+                msg = 'ok' if audio else 'audio file is empty !!!'
+            if msg != 'ok':
+                vad_result['code'] = 4003
+                vad_result['msg'] = msg
+                return
+            if not audio:
+                vad_result['code'] = 4002
+                vad_result['msg'] = 'no audio data'
+                return
+            # 音频vad
+            b = time.time()
+            total = vad_duration(audio)
+            logger.info(f'vad time: {time.time() - b}, {total = }')
+            vad_result['data'] = json.dumps(total)
+    except Exception as e:
+        logger.exception(e)
+        vad_result['code'] = 4000
+        vad_result['msg'] = str(e)
+    return vad_result
